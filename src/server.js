194723f5a7ee922abc3b409fbfedd700ca3c1f13
Or Ashtamker <ashi@MacBook-Pro.local>
2021-05-03 15:27:47 +0300

    some installation

--- src/server.js
+++ src/server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
 const app = express();
 const path = require('path');
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
 app.get('/api/getUser', (req,res)=>{
     const user = 'Or';
     res.json(user);
