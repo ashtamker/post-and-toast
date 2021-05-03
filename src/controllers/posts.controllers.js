df8742fc3c056e86d82c5540d6b309ae6f4f4f9e
Or Ashtamker <ashi@MacBook-Pro.local>
2021-05-04 01:07:05 +0300

    post route

--- src/controllers/posts.controllers.js
+++ src/controllers/posts.controllers.js
@@ -1,6 +1,23 @@
-
+const PostModel = require('../models/postMessage');
 
 
 exports.getPosts = async (req, res) => {
-    res.send('good joob');
+    try {
+        const postModel = await PostModel.find();
+        res.status(200).json(postModel)
+    } catch (error) {
+        res.status(404).json({message: error.message});
+    }
+
+}
+
+exports.createPost = async (req, res) => {
+    const post = req.body;
+    const newPost = new PostModel(post)
+    try {
+        await newPost.save()
+        res.status(200).json(newPost)
+    } catch (error) {
+        res.status(404).json({message: error.message});
+    }
 }
--- src/routes/posts.route.js
+++ src/routes/posts.route.js
@@ -3,7 +3,8 @@ const express = require('express');
 const router = express.Router();
 const postController = require('../controllers/posts.controllers');
 
-router.get('/', postController.getPosts)
-
+router
+.get('/', postController.getPosts)
+.post('/', postController.createPost)
  
 module.exports = router;
