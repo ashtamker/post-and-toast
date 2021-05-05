c76774fd26501147a315dac319cddc640060c626
Or Ashtamker <ashi@MacBook-Pro.local>
2021-05-05 11:24:12 +0300

    add upload option

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-// import FileBase from 'react-file-base64';
+import FileBase from 'react-file-base64';
 
 const Form = () => {
     const [postData, setPostData] = useState({
@@ -17,9 +17,9 @@ const Form = () => {
             <input name="title" label="title" value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value})}></input>
             <input name="message" label="message" value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value})}></input>
             <input name="tags" label="tags" value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value})}></input>
-            {/* <FileBase
+            <FileBase
             type="file" multiple={false} onDone={({base64}) => setPostData({ ...postData, selectedFile: base64})}
-            /> */}
+            />
         </form>
         </div>
     )
