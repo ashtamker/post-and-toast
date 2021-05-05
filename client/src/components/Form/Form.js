45a85ebbc55f6ba767401f0e7c51e7e986c188dd
Or Ashtamker <ashi@MacBook-Pro.local>
2021-05-05 13:53:22 +0300

    post handler

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,25 +1,42 @@
 import React, { useState } from 'react';
 import FileBase from 'react-file-base64';
+import { useDispatch } from 'react-redux';
+import './form.css';
+
+import { createPost } from '../../actions/posts';
 
 const Form = () => {
     const [postData, setPostData] = useState({
         creator: '', title: '', message: '', tags: '', selectedFile: ''
     })
-    const handleSubmit = () => {
+    const dispatch = useDispatch();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        dispatch(createPost(postData));
+    }
+
+    const clear = () => {
 
     }
 
     return (
         <div>
-        <form autoComplete="off" noValidate className="main-form" onSubmit={handleSubmit}>
             <h5>Share a picture or recipe</h5>
+        <form autoComplete="off" noValidate className="main-form" onSubmit={handleSubmit}>
+            
             <input name="creator" label="creator" value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value})}></input>
             <input name="title" label="title" value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value})}></input>
             <input name="message" label="message" value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value})}></input>
             <input name="tags" label="tags" value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value})}></input>
+            <div className="main-form">
             <FileBase
             type="file" multiple={false} onDone={({base64}) => setPostData({ ...postData, selectedFile: base64})}
             />
+            <button>Submit</button>
+            <button onClick={clear}>Clear</button>
+            </div>
         </form>
         </div>
     )
