46395a2c4dfc5f9d3a5ea8b1c6ef05decf373e8b
Or Ashtamker <ashi@MacBook-Pro.local>
2021-05-06 14:31:14 +0300

    post 2

--- client/src/components/Posts/post/Post.js
+++ client/src/components/Posts/post/Post.js
@@ -28,6 +28,17 @@ const Post = ({post}) => {
           <CardContent>
             <Typography className={classes.title} variant="h5" gutterBottom>{post.message}</Typography>
           </CardContent>
+          <CardActions className={classes.cardActions}>
+            <Button size="small" color="primary" onClick={() => {}}>
+                <ThumbUpAltIcon fontSize="small" />
+                Like
+                {post.likeCount}
+            </Button>
+            <Button size="small" color="primary" onClick={() => {}}>
+                <DeleteIcon fontSize="small" />
+                Delete
+            </Button>
+          </CardActions>
       </Card>
     )
 }
