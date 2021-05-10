b5b92bb3a0379b84e35d254e0adb0bcece5aa0b6
Or Ashtamker <ashi@MacBook-Pro.local>
2021-05-10 13:01:59 +0300

    style

--- client/src/AppStyle.js
+++ client/src/AppStyle.js
@@ -1,7 +1,7 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 
-export default makeStyles(() => ({
+export default makeStyles((theme) => ({
     appBar: {
 
         borderRadius: 15,
@@ -17,6 +17,12 @@ export default makeStyles(() => ({
       image: {
         marginLeft: '15px',
       },
+      [theme.breakpoints.down('sm')]: {
+        mainContainer: {
+          flexDirection: "column-reverse",
+        }
+      } 
+      
 
 
 }));
