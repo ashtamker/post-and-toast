42d55fad5ded9332681c519572246ea3755a869a
Or Ashtamker <ashi@MacBook-Pro.local>
2021-05-16 18:38:22 +0300

    login controller

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -21,7 +21,25 @@ const signin = async (req, res) => {
 }
 
 const signup = async (req, res) => {
+    const {email, password, confirmPassword, firstName, lastName} = req.body;
+
+    try {
+        const searchUser = await User.findOne({email});
+        if(searchUser) return res.status(400).json({massage: "User exist wuth this email"});
+        
+        if(password !== confirmPassword) return res.status(400).json({massage: "Password don't match"});
     
+        const hashedPassword = await bcrypt.hash(password, 12);
+        const result = await User.create({ email, password: hashedPassword, name: `${firstName} ${lastName}`});
+        const token = jwt.sign({email: result.email, id:result._id}, 'test', {expiresIn: "1h"});
+
+        res.status(200).json({result, token});
+
+    } catch (error) {
+        res.status(500).json({massage: "Signup failed"}); 
+
+    }
+
 }
 
 module.exports = {
