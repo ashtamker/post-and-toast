e21aa76f70627863a89846a9191349cc31b4807e
Or Ashtamker <ashi@MacBook-Pro.local>
2021-05-18 01:55:27 +0300

    ./

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,6 +3,13 @@ import axios from 'axios';
 
 const URL = axios.create({ baseURL: 'http://localhost:8000'});
 
+URL.interceptors.request.use((req) => {
+    if(localStorage.getItem('profile')) {
+        req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
+    }
+    return req;
+});
+
 export const fetchPosts = () => URL.get('/posts');
 export const createPost = (newPost) => URL.post('/posts', newPost);
 export const updatePost = (id, updatedPost) => URL.patch(`/posts/${id}`, updatedPost);
