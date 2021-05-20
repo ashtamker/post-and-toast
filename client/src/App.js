c2f7203ee36fcacdb350e8098792b4215af9b0e0
Or Ashtamker <ashi@MacBook-Pro.local>
2021-05-20 13:26:03 +0300

    pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import useStyles from './AppStyle';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import { Container } from '@material-ui/core';
 import Navbar from './components/Navbar/Navbar';
 import Loading from './components/Loading/Loading';
 import Home from './components/Home/Home';
+import PostDetails from './components/postDetails/PostDetails';
 import Auth from './components/Auth/Auth';
 
 
@@ -12,8 +13,7 @@ import Auth from './components/Auth/Auth';
 function App() {
 
 const [loading, setLoading] = useState(true);  
-
-
+const user = JSON.parse(localStorage.getItem('profile'));
 
 useEffect(() => {
   setTimeout(() => {
@@ -23,28 +23,20 @@ useEffect(() => {
 
 
   return (
-    // <BrowserRouter>
-    //   <Container maxWidth="lg">
-    //       <Navbar />
-    //       <Switch>
-    //         <Route path="/" exact component={Home} />
-    //         <Route path="/auth" exact component={Auth} />
-    //       </Switch>
-    //   </Container>
-    // </BrowserRouter>
-    //////////////////////
-    
-    
+  
     <div>
     {loading === true?
       <Loading />: 
       
       <BrowserRouter>
-          <Container maxWidth="lg">
+          <Container maxWidth="xl">
               <Navbar />
               <Switch>
-                <Route path="/" exact component={Home} />
-                <Route path="/auth" exact component={Auth} />
+                <Route path="/" exact component={() => <Redirect to="/posts" />} />
+                <Route path="/posts" exact component={Home} />
+                <Route path="/posts/search" exact component={Home} />
+                <Route path="/posts/:id" exact component={PostDetails} />
+                <Route path="/auth" exact component={() => (!user ? <Auth /> : <Redirect to="/posts" />)} />
               </Switch>
           </Container>
       </BrowserRouter>
---
+++ client/src/components/pagination/Pagination.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {PaginationItem, Pagination} from '@material-ui/lab';
+import {Link} from 'react-router-dom';
+import useStyle from './style';
+
+const Pages = () => {
+    const classes = useStyle();
+    
+    return (
+        <Pagination 
+            classes={{ul:classes.ul}}
+            count={5}
+            page={1}
+            variant="outline"
+            color="primary"
+            renderItem={(item) => (
+        <PaginationItem {...item} component={Link} to={`/post?page=${1}`} />
+            )}
+        />
+    )
+}
+
+export default Pages;
